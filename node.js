f7a479f3b491d9a295d695693e5dbd72d7b419cb
RaviKumar <ravikumarainapudi97@gmail.com>
2024-09-05 21:16:19 +0530

    Hello World! Welcome to Node JS

--- node.js
+++ node.js
@@ -2,5 +2,5 @@ var http = require('http');
 
 http.createServer(function (req,res){
     res.writeHead(200, {'content-Type' : 'text/plani'});
-    res.end('Hello World!');
+    res.end('Hello World! Welcome to Node JS');
 }).listen(8080);

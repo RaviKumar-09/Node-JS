8072e8e52970c060204760fa7150fc4400be340f
RaviKumar <ravikumarainapudi97@gmail.com>
2024-09-06 11:03:23 +0530

    node currnet version

--- node.js
+++
@@ -1,6 +0,0 @@
-var http = require('http');
-
-http.createServer(function (req,res){
-    res.writeHead(200, {'content-Type' : 'text/plani'});
-    res.end('Hello World! Welcome to Node JS');
-}).listen(8080);

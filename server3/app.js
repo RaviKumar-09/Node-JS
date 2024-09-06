e1589061d312fcdf5aae5f4ad8cb9f776e4ff88e
RaviKumar <ravikumarainapudi97@gmail.com>
2024-09-06 12:11:07 +0530

    ERR_INVALID_HTTP_TOKEN'

--- server3/app.js
+++ server3/app.js
@@ -5,7 +5,7 @@ var http = require('http');
 
 http.createServer(function(req, res){
 
-    res.writeHead(200, {'content-Type ': 'text/html'})
+    res.writeHead(200, {'Content-Type ': 'text/html'})
     res.end('<h1>Hello world!!</h1>');
 
 

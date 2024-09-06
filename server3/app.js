43b804eb14b23b324e45dfd3ffb914f35c23e11e
RaviKumar <ravikumarainapudi97@gmail.com>
2024-09-06 12:09:54 +0530

    Mention the cotent type of the server

---
+++ server3/app.js
@@ -0,0 +1,12 @@
+// Mention the cotent type of the server
+
+var http = require('http');
+
+
+http.createServer(function(req, res){
+
+    res.writeHead(200, {'content-Type ': 'text/html'})
+    res.end('<h1>Hello world!!</h1>');
+
+
+}).listen(8080)
